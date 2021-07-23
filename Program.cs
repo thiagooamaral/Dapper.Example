a26c5d62992ae1a11dcec2f25740c8efd66d10c5
Thiago Amaral <thiago.amaral@scanbrazilconsulting.com>
2021-07-23 11:46:47 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Blog.Models;
+using Blog.Repositories;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+    class Program
+    {
+        private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=thor";
+
+        static void Main(string[] args)
+        {
+            var connection = new SqlConnection(CONNECTION_STRING);
+            connection.Open();
+
+            // ReadUsers(connection);
+            ReadUsersWithRoles(connection);
+            // ReadRoles(connection);
+            // ReadTags(connection);
+            
+            connection.Close();
+        }
+
+        public static void ReadUsers(SqlConnection connection)
+        {
+            var repository = new Repository<User>(connection);
+            var items = repository.Get();
+
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
+        }
+
+        public static void ReadUsersWithRoles(SqlConnection connection)
+        {
+            var repository = new UserRepository(connection);
+            var items = repository.GetWithRoles();
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Name);
+
+                foreach (var role in item.Roles)
+                    Console.WriteLine($" - {item.Name}");
+            }
+                
+        }
+
+        public static void ReadRoles(SqlConnection connection)
+        {
+            var repository = new Repository<Role>(connection);
+            var items = repository.Get();
+
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
+        }
+
+        public static void ReadTags(SqlConnection connection)
+        {
+            var repository = new Repository<Tag>(connection);
+            var items = repository.Get();
+
+            foreach (var item in items)
+                Console.WriteLine(item.Name);
+        }
+    }
+}
